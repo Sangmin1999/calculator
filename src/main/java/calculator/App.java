a2d7a8514f65052634c4a8d3a045df862d1f72ac
Sangmin Lee <sart5534@gmail.com>
2024-07-24 17:33:46 +0900

    Level2 - charAt();을 이용하여 char 타입으로 변환하여 문자열의 첫 번째 단어의 첫 번째 문자를 출력

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -13,7 +13,11 @@ public class App {
         System.out.print("두 번째 숫자를 입력하세요: ");
         int num2 = sc.nextInt();
 
-        System.out.println("num1, num2 = " + num1 + ", " + num2);
+        System.out.print("사칙연산 기호를 입력하세요: "); // +,-,*,/
+        // charAt(idx) : charAt 메서드는 매개변수로 char 타입으로 변환 하고자하는 문자열의 위치를 받는다.
+        char operator = sc.next().charAt(0);
+
+        System.out.println("operator = " + operator);
 
 
     }
