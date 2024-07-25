7d802cc5108fad4d0d7881c3d466a28dc57e7fb4
Sangmin Lee <sart5534@gmail.com>
2024-07-25 20:48:22 +0900

    Level1-3 - switch문을 이용한 사칙연산을 사용하여 연산을 진행 후 결과값을 출력

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -10,15 +10,41 @@ public class App {
         System.out.print("첫 번째 숫자를 입력하세요: ");
         //Scanner를 사용하여 양의 정수를 입력받고 변수에 저장합니다.
         int num1 = sc.nextInt();
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        int num2 = sc.nextInt();
 
         System.out.print("사칙연산 기호를 입력하세요: "); // +,-,*,/
         // charAt(idx) : charAt 메서드는 매개변수로 char 타입으로 변환 하고자하는 문자열의 위치를 받는다.
         char operator = sc.next().charAt(0);
 
-        System.out.println("operator = " + operator);
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int num2 = sc.nextInt();
 
+        int result = 0; // switch문에서 result 변수를 사용하기위해 선언
+
+        switch (operator) { // switch 문을 통해서 사칙연산 기호를 입력했을때 4가지 경우를 만들어준다
+            case '+' :
+                result = num1 + num2;
+                System.out.println("결과: " + result);
+                break;
+            case '-' :
+                result = num1 - num2;
+                System.out.println("결과: " + result);
+                break;
+            case '*' :
+                result = num1 * num2;
+                System.out.println("결과: " + result);
+                break;
+            case '/' :
+                if (num2 == 0) { // 나눗셈 연산에서 두번째 정수에 0이 입력되는 경우에 출력
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                    break;
+                }
+                result = num1 / num2;
+                System.out.println("결과: " + result);
+                break;
+            default:
+                System.out.println("정확한 사칙연산 기호를 입력해주세요.");
+                break;
+        }
 
     }
 
